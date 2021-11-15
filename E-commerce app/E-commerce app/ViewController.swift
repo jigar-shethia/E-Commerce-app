2f2ef752fb1cbc22e7e35334e03da94392d15846
jigar.shethia01 <jigar.shethia@inscripts.com>
2021-11-15 18:22:14 +0530

    first commit

---
+++ E-commerce app/E-commerce app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  E-commerce app
+//
+//  Created by Jigar Shethia on 15/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ E-commerce app/E-commerce appTests/E_commerce_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  E_commerce_appTests.swift
+//  E-commerce appTests
+//
+//  Created by Jigar Shethia on 15/11/21.
+//
+
+import XCTest
+@testable import E_commerce_app
+
+class E_commerce_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
